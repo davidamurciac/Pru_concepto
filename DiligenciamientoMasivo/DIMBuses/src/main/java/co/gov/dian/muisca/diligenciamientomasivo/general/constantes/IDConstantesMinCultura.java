cf22ec3912d0e17a13fced443c0ec39da2945a5c
David Andres Murcia Castro <dmurciac1@dian.gov.co>
2019-11-01 14:57:18 -0500

    commit

--- DiligenciamientoMasivo/DIMBuses/src/main/java/co/gov/dian/muisca/diligenciamientomasivo/general/constantes/IDConstantesMinCultura.java
+++
@@ -1,7 +0,0 @@
-package co.gov.dian.muisca.diligenciamientomasivo.general.constantes;
-
-public interface IDConstantesMinCultura {
-	public static final String NOMBRE_TAG_RAIZ = "resp_mincultura";
-	
-	Integer VAL_DOMINIO_CORRECCION_PRIVADA = 1;
-}
